00923b65325a03aabd76542ee3479cc803a455d3
Matt Bell <matthewaustinbell@gmail.com>
2019-04-18 09:03:41 -0500

    added button

--- js/helpers/eventhandlers.js
+++ js/helpers/eventhandlers.js
@@ -1,5 +1,12 @@
 import eventListeners from './sandwich.js'
 
+//Notes
+//create a variable with name checkBoxes 
+// assign it the value of document.getElementsByClassName('form-check-input')
+// for loop through checkBoxes with i<checkBoxes.length
+// on each loop check if box is checked
+// if so, add ingredient to order array and
+
 const addCheckEventListener = () => {
     const checkBoxes = document.getElementsByClassName('checkBoxes')
     for (let i=0; i<checkBoxes.length; i++){
